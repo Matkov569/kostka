1450f1c20d97e99b6a56df7039c52044539c6c02
Matkov569 <44322302+Matkov569@users.noreply.github.com>
2019-10-09 22:02:15 +0200

    Update kostka.py

--- kostka.py
+++ kostka.py
@@ -1,12 +1,50 @@
-# https://runmycode.online
 
 # -*- coding: utf-8 -*-
 
 import random
 
-oczka = random.randint(1,6)
-
-print("Symulator rzutu kostką")
+print("Symulator rzutu kostka")
 
 print ("Losowanie...")
 
+oczka = random.randint(1,6)
+
+print("Wylosowano: " , oczka)
+
+border= "X" * 9
+
+if oczka == 1:
+    row1 = "X" + "       " + "X"
+    row2 = "X" + "   O   " + "X"
+    row3 = "X" + "       " + "X"
+
+elif oczka == 2:
+    row1 = "X" + " O     " + "X"
+    row2 = "X" + "       " + "X"
+    row3 = "X" + "     O " + "X"
+
+elif oczka == 3:
+    row1 = "X" + " O     " + "X"
+    row2 = "X" + "   O   " + "X"
+    row3 = "X" + "     O " + "X"
+
+elif oczka == 4:
+    row1 = "X" + " O   O " + "X"
+    row2 = "X" + "       " + "X"
+    row3 = "X" + " O   O " + "X"
+
+elif oczka == 5:
+    row1 = "X" + " O   O " + "X"
+    row2 = "X" + "   O   " + "X"
+    row3 = "X" + " O   O " + "X"
+
+else:
+    row1 = "X" + " O   O " + "X"
+    row2 = "X" + " O   O " + "X"
+    row3 = "X" + " O   O " + "X"
+
+print(border)
+print(row1)
+print(row2)
+print(row3)
+print(border)
